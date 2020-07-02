2e3c8cbca41b8fbdd047bb5d000bcaea64f23dd8
xamlor <villatte.maxime@orange.fr>
2020-07-02 23:56:02 +0200

    Last view correction v3

--- src/app/controllers/BoardController.java
+++ src/app/controllers/BoardController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 
 import javafx.scene.input.MouseEvent;
